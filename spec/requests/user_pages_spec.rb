3d40fa7dbf7b6be8bd331b4317376da2760500e7
Eva Perales <eperales@gnuine.com>
2013-02-08 17:10:52 +0100

    Chapter 5 exercices

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,9 +4,9 @@ describe "User pages" do
 
     subject {page}
 
-    describe "Singup page" do
+    describe "Signup page" do
         before {visit signup_path}
-        it { should have_selector('h1', :text => 'Sing up') }
-        it { should have_selector( 'title', :text => full_title('Sing up')) }
+        it { should have_selector('h1', :text => 'Sign up') }
+        it { should have_selector( 'title', :text => full_title('Sign up')) }
     end
 end
